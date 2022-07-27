d47ccfef18f7dd8e7aba31536fc20d9fe543ac64
cse18107 <cse18107@cemk.ac.in>
2022-07-27 13:12:34 +0530

    adding event bus and query

--- comments/index.js
+++ comments/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
+const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 
 let commentsByPostId = {};
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
     const { content } = req.body;
 
@@ -16,12 +18,26 @@ app.post("/posts/:id/comments", (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content,
+        postId: req.params.id
+      }
+    })
+
     res.status(201).send(comments);
 
 });
 
 app.get("/posts/:id/comments", (req, res) => {
   res.status(200).send(commentsByPostId[req.params.id]  || [] );
+});
+
+app.post('/events', (req,res) => {
+  console.log('Received Event',req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
